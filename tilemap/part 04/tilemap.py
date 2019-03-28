f2b96e9a4e5bbd7024e85be28060bb304b4dd755
Lanranyan <44810787+Lanranyan@users.noreply.github.com>
2019-03-28 08:45:51 -0700

    Update tilemap.py

--- tilemap/part 04/tilemap.py
+++ tilemap/part 04/tilemap.py
@@ -6,12 +6,15 @@ class Map:
         self.data = []
         with open(filename, 'rt') as f:
             for line in f:
-                self.data.append(line.strip())
+                self.data.append(line)
 
         self.tilewidth = len(self.data[0])
         self.tileheight = len(self.data)
+        # How many tiles wide and tall our map is
+        # keeps track of how big our map is (width, height)
         self.width = self.tilewidth * TILESIZE
         self.height = self.tileheight * TILESIZE
+        #pixel width
 
 class Camera:
     def __init__(self, width, height):
@@ -21,14 +24,14 @@ class Camera:
 
     def apply(self, entity):
         return entity.rect.move(self.camera.topleft)
+        # the move command, when applied to rect, gives a new rect
+        # that's shifted by the camera.topleft
 
     def update(self, target):
         x = -target.rect.x + int(WIDTH / 2)
+            #adds half the screen size
         y = -target.rect.y + int(HEIGHT / 2)
 
         # limit scrolling to map size
-        x = min(0, x)  # left
-        y = min(0, y)  # top
-        x = max(-(self.width - WIDTH), x)  # right
-        y = max(-(self.height - HEIGHT), y)  # bottom
+        x = min(0, x)
         self.camera = pg.Rect(x, y, self.width, self.height)
